1eef3efb7b731ae70b3ab80c2f2a83484e7115b1
Opilane TTHK <opilane@tthk.ee>
2023-03-31 09:46:03 +0300

    used optical flow algoritm in movement detection

--- main.py
+++ main.py
@@ -3,11 +3,10 @@ import numpy as np
 from picamera.array import PiRGBArray
 from picamera import PiCamera
 import time
-import tensorflow as tf
-  
-import tensorflow.keras as keras
+#import tensorflow as tf
+#import tensorflow.keras as keras
 
-
+"""
 def identifyImage():
     model = keras.models.load_model("96%90%")
     image_size = (180,180)
@@ -17,9 +16,7 @@ def identifyImage():
     numbers = predictions[0]
     for i in range(len(class_names)):
         print(class_names[i], " : " , round(numbers[i].astype(float)* 100, 2),"%")
-
-
-
+"""
 def main(use_usb_camera=False):
     if use_usb_camera:
         camera = cv2.VideoCapture(0)
@@ -30,7 +27,7 @@ def main(use_usb_camera=False):
         camera.framerate = 24
     raw_image = PiRGBArray(camera, size=(640,480))
     time.sleep(0.1)
-    first_frame = None
+    prev_gray = None
     for frame in camera.capture_continuous(raw_image, format="bgr", use_video_port=True):
         kernel = np.ones((20,20),np.uint8)
         img = frame.array
@@ -39,62 +36,43 @@ def main(use_usb_camera=False):
         gray = cv2.morphologyEx(gray,cv2.MORPH_CLOSE,kernel)
         # Remove salt and pepper noise with a median filter
         gray = cv2.medianBlur(gray,5)
-        if first_frame is None:
-            first_frame = gray
-            raw_image.truncate(0)
-            continue
-            
-        absolute_difference = cv2.absdiff(first_frame,gray)
-            
-        _, absolute_difference = cv2.threshold(absolute_difference, 100, 255, cv2.THRESH_BINARY)
 
-        contours, hierarchy = cv2.findContours(absolute_difference,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)[-2:]
-        areas = [cv2.contourArea(c) for c in contours]
-        if len(areas) < 1:
-            print("no movement detected")
-            cv2.imshow('Frame',img)
-            key = cv2.waitKey(1) & 0xFF
-            raw_image.truncate(0)
+        if prev_gray is not None:
+            # Calculate the optical flow between the previous frame and the current frame
+            flow = cv2.calcOpticalFlowFarneback(prev_gray, gray, None, 0.5, 3, 15, 3, 5, 1.2, 0)
+            # Calculate the magnitude and direction of the flow vectors
+            magnitude, angle = cv2.cartToPolar(flow[..., 0], flow[..., 1])
 
-            if key == ord("q"):
-                break
-            # Go to the top of the for loop
-            continue
-        else:
-            # Find the largest moving object in the image
-            max_index = np.argmax(areas)
-            # Draw the bounding box
-        cnt = contours[max_index]
-        x,y,w,h = cv2.boundingRect(cnt)
-        cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,0),3)
-        # Draw circle in the center of the bounding box
-        x2 = x + int(w/2)
-        y2 = y + int(h/2)
-        cv2.circle(img,(x2,y2),4,(0,255,0),-1)
-        # Print the centroid coordinates (we'll use the center of the
-        # bounding box) on the image
-        text = "x: " + str(x2) + ", y: " + str(y2)
-        cv2.putText(img, text, (x2 - 10, y2 - 10),
-        cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
-        # Display the resulting frame
-        cv2.imshow("Frame",img)
+            # Set the mask to display only the motion that is above a certain threshold
+            mask = np.zeros_like(img)
+            mask[..., 1] = 255
+            mask[..., 0] = np.where(magnitude > 30, 255, 0)
+
+            # Find contours in the mask image
+            contours, _ = cv2.findContours(mask[..., 0], cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+            # Draw a bounding box around each contour
+            for contour in contours:
+                (x, y, w, h) = cv2.boundingRect(contour)
+                cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+            # Display the resulting frame
+            cv2.imshow('Frame', img)
+
+        prev_gray = gray
+
         # Wait for keyPress for 1 millisecond
         key = cv2.waitKey(1) & 0xFF
         # Clear the stream in preparation for the next frame
         raw_image.truncate(0)
 
-        print("movement detected")
-        camera.capture("test.jpeg")
-        identifyImage() 
-        # If "q" is pressed on the keyboard, 
-        # exit this loop
         if key == ord("q"):
             break
+
     if use_usb_camera:
         camera.release()
     else:
         camera.close()
-
 
 if __name__ == "__main__":
     main()
